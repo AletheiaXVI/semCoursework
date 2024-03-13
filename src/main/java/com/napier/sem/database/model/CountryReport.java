f4830058f21967e4224113f0346f30af33bd8dc7
sheatait02 <shea66755@gmail.com>
2024-03-13 13:03:27 +0000

    Removed Continent model class in lieu of using strings to store continents instead of enum.
    Implements new class model called 'country report' for taking specific query information for reports to be outputted.
    
    First query for country reports created & working.

---
+++ src/main/java/com/napier/sem/database/model/CountryReport.java
@@ -0,0 +1,61 @@
+package com.napier.sem.database.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Represents the output for country reports from a SQL query
+ */
+public class CountryReport {
+    private final String code;
+    private final String name;
+    private final String continent;
+    private final String region;
+    private final int population;
+    private final int capital;
+
+    public CountryReport(ResultSet rs) {
+        try {
+            this.code = rs.getString("Code");
+            this.name = rs.getString("Name");
+            this.continent = rs.getString("Continent");
+            this.region = rs.getString("Region");
+            this.population = rs.getInt("Population");
+            this.capital = rs.getInt("Capital");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContinent() { return continent; }
+    public String getRegion() {
+        return region;
+    }
+    public int getPopulation() {
+        return population;
+    }
+
+    public int getCapital() {
+        return capital;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryReport{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", continent=" + continent + '\'' +
+                ", region='" + region + '\'' +
+                ", population=" + population +
+                ", capital=" + capital +
+                '}';
+    }
+}
