3fe1a70b02ec1213133c6eafad6bae9bf4b3b82a
James Vincent <40579227@live.napier.ac.uk>
2024-04-17 20:22:15 +0100

    Updated CSVExport.java

--- src/main/java/com/napier/sem/util/CSVExport.java
+++ src/main/java/com/napier/sem/util/CSVExport.java
@@ -5,11 +5,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import com.napier.sem.database.DatabaseConnection;
 import com.napier.sem.database.ObjectMapper;
-import com.napier.sem.database.model.CountryReport;
+import com.napier.sem.database.model.City;
+import com.napier.sem.database.model.Country;
 
 
 /**
@@ -38,24 +40,6 @@ public class CSVExport {
     private static final String COMMA = ","; // Delimiter for separating fields in CSV
     private static final String DOUBLE_QUOTES = "\""; // Character used to enclose fields in CSV
     private static final String EMBEDDED_DOUBLE_QUOTES = "\"\""; // Escape sequence for quotes within a field
-
-    /**
-     * Formats CountryReport object into a string array to be outputted into a .CSV report
-     *
-     * @param country - instance of CountryReport
-     * @return the String array for use in writing to .CSV report
-     *
-     */
-    public static String[] countryReportToStringArray(CountryReport country) {
-        return new String[] {
-                country.getCode(),
-                country.getName(),
-                country.getContinent(),
-                country.getRegion(),
-                String.valueOf(country.getPopulation()),
-                String.valueOf(country.getCapital()),
-        };
-    }
 
     /**
      * Formats a field for CSV by adding quotes if it has special characters.
@@ -117,14 +101,128 @@ public class CSVExport {
 
         init(); // Connects to the database
 
-        List<CountryReport> countries = objectMapper.getObjectsFromDatabase("SELECT * FROM country", CountryReport::new);
 
-        List<String[]> data = new ArrayList<>();
-        for (CountryReport countryReport : countries) {
-            data.add(countryReportToStringArray(countryReport));
+        // Country Report Stuff
+        // Function to convert Country object to String array for CSV
+        Function<Country, String[]> countryToStringArray = country -> new String[]{
+                country.getCode(),
+                country.getName(),
+                country.getContinent(),
+                country.getRegion(),
+                String.valueOf(country.getPopulation()),
+                String.valueOf(country.getCapital()),
+                country.getCode2()
+        };
+
+        //Retrieve countries and convert to CSV data
+        List<Country> countries = objectMapper.getObjectsFromDatabase("SELECT * FROM country", Country::new);
+
+        List<String[]> countryData = new ArrayList<>();
+        for (Country country : countries) {
+            countryData.add(countryToStringArray.apply(country));
         }
 
         CSVExport writerCountry = new CSVExport();
-        writerCountry.writeToCSV(data, "Countries.csv");
+        writerCountry.writeToCSV(countryData, "Countries.csv");
+
+        // City Report Stuff
+
+        // Function to convert City object to String array for CSV
+        Function<City, String[]> cityToStringArray = city -> new String[]{
+                String.valueOf(city.getId()),
+                city.getName(),
+                city.getCountryCode(),
+                city.getDistrict(),
+                String.valueOf(city.getPopulation()),
+        };
+
+        // Retrieve cities and convert to CSV data
+        List<City> cities = objectMapper.getObjectsFromDatabase("SELECT * FROM city", City::new);
+
+        List<String[]> cityData = new ArrayList<>();
+        for (City city : cities) {
+            cityData.add(cityToStringArray.apply(city));
+        }
+
+        CSVExport writerCity = new CSVExport();
+        writerCity.writeToCSV(cityData, "Cities.csv");
+
+        // Capital City Report Stuff
+        // Function to extract capital city data from Country object
+        Function<Country, String[]> countryToCapitalArray = country -> {
+            int capitalCityID = country.getCapital();
+
+            if (capitalCityID <= 0) {
+                return new String[]{"", "", ""}; // Handle invalid ID
+            }
+
+            List<City> potentialCapitals = objectMapper.getObjectsFromDatabase("SELECT * FROM city", City::new);
+            City capitalCity = potentialCapitals.stream()
+                    .filter(city -> city.getId() == capitalCityID)
+                    .findFirst()
+                    .orElse(null);
+
+            if (capitalCity == null) {
+                return new String[]{"", "", ""}; // Handle missing capital
+            }
+
+            String capitalName = capitalCity.getName();
+            String countryName = country.getName();
+            long capitalPopulation = capitalCity.getPopulation();
+
+            return new String[]{capitalName, countryName, String.valueOf(capitalPopulation)};
+        };
+
+        // Retrieve countries and process capital city data
+        List<Country> capitals = objectMapper.getObjectsFromDatabase("SELECT * FROM country", Country::new);
+
+        List<String[]> capitalData = new ArrayList<>();
+        for (Country capital : capitals) {
+            capitalData.add(countryToCapitalArray.apply(capital));
+        }
+
+        CSVExport writerCapital = new CSVExport();
+        writerCapital.writeToCSV(capitalData, "Capitals.csv");
+
+        // Population Report Stuff
+
+        // Function to calculate urban and rural population percentages
+        Function<Country, String[]> countryToPopulationData = country -> {
+            int totalPopulation = country.getPopulation();
+
+            // Find total city population for the country
+            List<City> allCities = objectMapper.getObjectsFromDatabase(
+                    "SELECT * FROM city WHERE CountryCode = ?", City::new, country.getCode()
+            );
+            int cityPopulation = allCities.stream().mapToInt(City::getPopulation).sum();
+
+            // Calculate rural population
+            int ruralPopulation = totalPopulation - cityPopulation;
+
+            // Calculate percentages
+            double urbanPercentage = (double) cityPopulation / totalPopulation * 100;
+            double ruralPercentage = (double) ruralPopulation / totalPopulation * 100;
+
+            return new String[]{
+                    country.getContinent(),
+                    country.getRegion(),
+                    country.getName(),
+                    String.valueOf(totalPopulation),
+                    String.format("%.2f%%", urbanPercentage),
+                    String.format("%.2f%%", ruralPercentage),
+            };
+        };
+
+        // Retrieve countries and process population data
+        List<Country>allCountries = objectMapper.getObjectsFromDatabase("SELECT * FROM country", Country::new);
+
+        List<String[]> populationData = new ArrayList<>();
+        for (Country country : allCountries) {
+            populationData.add(countryToPopulationData.apply(country));
+        }
+
+        CSVExport writerPopulation = new CSVExport();
+        writerPopulation.writeToCSV(populationData, "Populations.csv");
+
     }
 }
